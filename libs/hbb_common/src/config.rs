69991b92007754dc9e4299cba6e82fa317d2ea9c
Yiwen Liu <43922414+gcbzq32l@users.noreply.github.com>
2024-10-17 18:52:09 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["fnos.112150.xyz"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["gcbzq32l.112150.xyz"];
 pub const PUBLIC_RS_PUB_KEY: &str = "G7fsj+SeqMJuZwBCwZWSaIAfT35HjMuvoSxmIsFPwZk=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
